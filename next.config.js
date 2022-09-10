61968fbfb1c0dba6c937c31d0d0e0b6faa75d451
Pedro Costa <pehcst@outlook.com.br>
2022-09-10 12:53:10 -0300

    google analytics

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     //prsmic
     NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN: process.env.PRISMIC_ACCESS_TOKEN,
     NEXT_PUBLIC_PRISMIC_API: process.env.PRISMIC_API,
+    NEXT_PUBLIC_GA_ID: process.env.GOOGLE_ANALYTICS,
   },
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,37 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider, Link } from '@chakra-ui/react'
 import { PrismicProvider } from '@prismicio/react'
-import { PrismicPreview } from '@prismicio/next'
 import { theme } from '../styles/theme'
 import '../styles/style.css'
-import { linkResolver, repositoryName } from '../services/prismic'
+import { linkResolver } from '../services/prismic'
+import * as gtag from '../lib/gtag'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+            }}
+          />
+        </>
+      )}
       <PrismicProvider
         linkResolver={linkResolver}
         internalLinkComponent={({ href, children, ...props }: any) => (
